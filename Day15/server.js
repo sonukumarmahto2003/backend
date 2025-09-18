d0420be8ce10f2c19d6209df900bb311cc3df299
sonukumar1S <sonukumarcs2003@gmail.com>
2025-09-18 10:55:49 +0530

    middleware

--- Day15/server.js
+++ Day15/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./src/routes/user.routes");
-const connectDB = require("./src/config/db");
+const connectDB = require("./src/config/db/db");
 const app = express();
 connectDB();
 app.use(express.json());
